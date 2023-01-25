a806883de6b4e53a1c3c90653223ae189139d9f2
mathildeew <mathildewiik@hotmail.com>
2023-01-25 14:06:01 +0100

    moved unautorized user into login file

--- cypress/e2e/testing/login.cy.js
+++ cypress/e2e/testing/login.cy.js
@@ -1,6 +1,6 @@
 const TEST_USER = {
-  email: "rordon@stud.noroff.no",
-  password: "Gurkemeie69",
+  email: "donkeys@stud.noroff.no",
+  password: "Test1234",
 };
 
 describe("Log in", () => {
@@ -17,5 +17,14 @@ describe("Log in", () => {
     cy.get("input[name=password]#loginPassword").type(TEST_USER.password);
     cy.get("button[type=submit").contains("Login").click();
     cy.wait(1000);
+  });
+
+  it("The user cannot submit the login form with invalid credentials and is shown a message.", () => {
+    cy.get("#registerForm button").contains("Login").click();
+    cy.get("input[name=email]#loginEmail").type(
+      "thiswillnotwork@stud.noroff.no"
+    );
+    cy.get("input[name=password]#loginPassword").type("PazZWoRdZ");
+    cy.get("button[type=submit").contains("Login").click();
   });
 });
--- cypress/e2e/testing/logout.cy.js
+++ cypress/e2e/testing/logout.cy.js
@@ -1,6 +1,6 @@
 const TEST_USER = {
-  email: "rordon@stud.noroff.no",
-  password: "Gurkemeie69",
+  email: "donkeys@stud.noroff.no",
+  password: "Test1234",
 };
 
 describe("Log out", () => {
@@ -9,7 +9,6 @@ describe("Log out", () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get("#registerForm button").contains("Login").click();
-    cy.wait(1000);
     cy.get("input[name=email]#loginEmail").type(TEST_USER.email);
     cy.get("input[name=password]#loginPassword").type(TEST_USER.password);
     cy.get("button[type=submit").contains("Login").click();
