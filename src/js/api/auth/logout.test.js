732bf2355ae4cf424abf134b8b7dbdf14fb3009a
mathildeew <mathildewiik@hotmail.com>
2023-01-25 20:57:15 +0100

    clean up logout jest test

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,14 +1,6 @@
 import { logout } from "./logout";
 
-const TEST_EMAIL = "donkeys@stud.noroff.no";
-const TEST_PASSWORD = "Test1234";
 const TEST_TOKEN = "theaveragelifeexpectancyofadonkeyisjustover30years";
-
-const TEST_JSON = {
-  email: TEST_EMAIL,
-  password: TEST_PASSWORD,
-  accessToken: TEST_TOKEN,
-};
 
 class LocalStorageMock {
   constructor() {
@@ -37,12 +29,10 @@ global.localStorage = new LocalStorageMock();
 localStorage.setItem("token", TEST_TOKEN);
 
 describe("Logout", () => {
+  expect(localStorage.getItem("token")).toEqual(TEST_TOKEN);
+
   it("Clears the token from browser storage", () => {
-    expect(localStorage.getItem("token")).toEqual(TEST_TOKEN);
     logout();
     expect(localStorage.getItem("token")).toBeUndefined();
-    // expect(localStorage.removeItem()).toHaveBeenCalled();
   });
 });
-
-// https://jestjs.io/docs/expect
